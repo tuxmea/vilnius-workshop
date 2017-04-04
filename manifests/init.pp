4d12c088529357ff04b2afa0826e75a69b5f50f2
Martin Alfke <ma@example42.com>
2017-04-04 21:58:51 +0200

    add a parameter
    used later for stdlib validate_bool so we can see how depending mosules
    work

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,15 @@
-class devopspro-vilnius {
+class devopspro-vilnius (
+  $validate = false,
+){
 package { 'less'
   ensure => present,
 }
   file { '/etc/lessrc':
   ensure => present
-  owner  => 'root',
+  owner    => 'root',
   source => 'puppet:///modules/devopspro-vilnius/lessrc.${::facts['os']['family']}',
-  }
+}
+if $validate {
+notify { 'validation enabled': }
+}
 }
